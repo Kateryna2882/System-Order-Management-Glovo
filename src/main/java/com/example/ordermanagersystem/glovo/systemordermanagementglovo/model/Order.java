f2b945897990a7fd4f87a996e7de8c4208832027
Kateryna2882 <govoryhina82@gmail.com>
2024-01-25 14:25:30 +0200

    Add fields and methods in the package model

--- src/main/java/com/example/ordermanagersystem/glovo/systemordermanagementglovo/model/Order.java
+++ src/main/java/com/example/ordermanagersystem/glovo/systemordermanagementglovo/model/Order.java
@@ -1,4 +1,40 @@
 package com.example.ordermanagersystem.glovo.systemordermanagementglovo.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Order {
+    private int orderId;
+    private List<Product>products;
+
+    public Order(int orderId){
+        this.orderId = orderId;
+        this.products = new ArrayList<>();
+    }
+
+    public void addProduct(Product product) {
+        products.add(product);
+    }
+
+    public List<Product> getProducts() {
+        return products;
+    }
+    public int getOrderId() {
+        return orderId;
+    }
+    public double calculateTotalPrice() {
+        double totalPrice = 0.0;
+        for (Product product : products) {
+            totalPrice += product.getPrice();
+        }
+        return totalPrice;
+    }
+    public void displayOrderInfo() {
+        System.out.println("Order ID: " + orderId);
+        System.out.println("Products:");
+        for (Product product : products) {
+            System.out.println("- " + product.getName() + ": $" + product.getPrice());
+        }
+        System.out.println("Total Price: $" + calculateTotalPrice());
+    }
 }
--- src/main/java/com/example/ordermanagersystem/glovo/systemordermanagementglovo/model/Product.java
+++ src/main/java/com/example/ordermanagersystem/glovo/systemordermanagementglovo/model/Product.java
@@ -1,4 +1,23 @@
 package com.example.ordermanagersystem.glovo.systemordermanagementglovo.model;
 
 public class Product {
-}
+        private String name;
+        private double price;
+
+        public Product(String name, double price) {
+            this.name = name;
+            this.price = price;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public double getPrice() {
+            return price;
+        }
+        public void displayProductInfo() {
+            System.out.println("Product: " + name);
+            System.out.println("Price: $" + price);
+        }
+    }
